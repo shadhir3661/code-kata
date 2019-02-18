a1f7c34854352276b799a77334c8bfa220935e63
shadhir3661 <47440873+shadhir3661@users.noreply.github.com>
2019-02-18 18:18:19 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -1,11 +1,11 @@
-num=int(raw_input())
+a=int(raw_input())
 sum = 0
-temp = num
+temp = a
 while temp > 0:
    digit = temp % 10
    sum += digit ** 3
    temp //= 10
-if num == sum:
+if a == sum:
    print("yes")
 else:
    print("no")
