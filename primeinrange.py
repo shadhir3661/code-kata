3a768d10a4e90580ecab5fcf1a5e46bb5cea203a
shadhir3661 <47440873+shadhir3661@users.noreply.github.com>
2019-02-18 15:12:40 +0530

    Create primeinrange.py

---
+++ primeinrange.py
@@ -0,0 +1,10 @@
+lower,upper=raw_input().split()
+lower=int(lower)
+upper=int(upper)
+for num in range(lower+1,upper):
+   if num > 1:
+       for i in range(2,num):
+           if (num % i == 0):
+               break
+       else:
+           print(num),
